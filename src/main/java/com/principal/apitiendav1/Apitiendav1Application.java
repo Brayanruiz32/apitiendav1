6e5212d46c609f5a14002a4474a315ea78f04f8b
Brayanruiz32 <maverick.brm@gmail.com>
2024-06-15 15:34:49 -0500

    Levantamiento de api

---
+++ src/main/java/com/principal/apitiendav1/Apitiendav1Application.java
@@ -0,0 +1,13 @@
+package com.principal.apitiendav1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apitiendav1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apitiendav1Application.class, args);
+	}
+
+}
